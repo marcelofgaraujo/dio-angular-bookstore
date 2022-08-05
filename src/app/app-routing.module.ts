98393c1a6a203fadbdd020f9106fe8371534ec54
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-08-05 14:43:43 -0300

    routing the page of application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: BookstoreAppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
